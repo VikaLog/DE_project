3f2b25761219f95a3d3490a4be60858ddb81a48c
MSI <MSI@MacBook-Pro-7.local>
2023-01-06 12:23:58 +0200

    some files are deleted

--- lesson_03/examples/clean_sales/ddl/sales_cleaned.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE TABLE my_dataset.sales_cleaned
-(
-  client STRING,
-  purchase_date DATE,
-  product STRING,
-  price INTEGER
-)
-;
--- lesson_03/examples/clean_sales/dml/clean_sales.sql
+++
@@ -1,8 +0,0 @@
-INSERT `my_dataset.sales_cleaned`(client, purchase_date, product, price)
-SELECT
-  client,
-  CAST(purchase_date AS DATE) AS purchase_date,
-  product,
-  CAST(RTRIM(price, 'USD') AS INTEGER) AS price
-FROM `my_dataset.sales_raw`
-;
