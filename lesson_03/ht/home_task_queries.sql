1460f1e92630b0292863d0c6de7ea752b0c2d3ed
MSI <MSI@MacBook-Pro-7.local>
2023-01-06 12:22:58 +0200

    comments are added

--- lesson_03/ht/home_task_queries.sql
+++ lesson_03/ht/home_task_queries.sql
@@ -9,12 +9,14 @@
 Результат відсортувати за спаданням.
 */
 SELECT
-    c.name AS category_name
-    ,COUNT(f.film_id) AS film_count
+    c.name                   AS category_name
+    ,COUNT(f.film_id)        AS film_count
 FROM
-    category c
-    INNER JOIN film_category fc ON c.category_id = fc.category_id
-    INNER JOIN film f ON fc.film_id = f.film_id
+    category                 AS c
+    INNER JOIN film_category AS fc
+        ON c.category_id = fc.category_id
+    INNER JOIN film          AS f
+        ON fc.film_id = f.film_id
 GROUP BY
     c.name
 ORDER BY
@@ -31,17 +33,21 @@ ORDER BY
 SELECT
 	CONCAT(a.last_name, ', ', a.first_name) AS actors
 FROM
-	actor a
-	INNER JOIN film_actor fa ON a.actor_id = fa.actor_id
-    INNER JOIN (SELECT
+	actor                                   AS a
+	INNER JOIN film_actor                   AS fa
+	    ON a.actor_id = fa.actor_id
+    INNER JOIN (
+                SELECT                            -- films with count of rents
                     i1.film_id
-                    ,COUNT(r1.rental_date) AS films_count
+                    ,COUNT(r1.rental_date)  AS films_count
                 FROM
-                    inventory i1
-                    INNER JOIN rental r1 ON i1.inventory_id = r1.inventory_id
+                    inventory               AS i1
+                    INNER JOIN rental       AS r1
+                        ON i1.inventory_id = r1.inventory_id
                 GROUP BY
                     i1.film_id
-                ) f_top ON f_top.film_id = fa.film_id
+                )                           AS f_top
+        ON f_top.film_id = fa.film_id
 ORDER BY
 	f_top.films_count DESC
 LIMIT 10
@@ -55,31 +61,44 @@ LIMIT 10
 в прокаті
 */
 SELECT
-    c3.name
+    c3.name                  AS film_category
 FROM
-    category c3
-    INNER JOIN film_category fc3 ON c3.category_id = fc3.category_id
-    INNER JOIN inventory i3 ON fc3.film_id = i3.inventory_id
-    INNER JOIN rental r3 ON i3.inventory_id = r3.inventory_id
-    INNER JOIN payment p3 ON p3.rental_id = r3.rental_id
+    category                 AS c3
+    INNER JOIN film_category AS fc3
+        ON c3.category_id = fc3.category_id
+    INNER JOIN inventory     AS i3
+        ON fc3.film_id = i3.inventory_id
+    INNER JOIN rental        AS r3
+        ON i3.inventory_id = r3.inventory_id
+    INNER JOIN payment       AS p3
+        ON p3.rental_id = r3.rental_id
 GROUP BY
     c3.name
-HAVING SUM(p3.amount) = (SELECT
-                             MAX(cam.category_amount)
+HAVING SUM(p3.amount) = (
+                         SELECT              -- select max amount of category
+                             MAX(cam.category_amount)        AS max_category_amount
                          FROM
-                             category c2
+                             category                        AS c2
                          INNER JOIN
-                                 (SELECT
-                                    1.category_id
-                                    ,SUM(p1.amount) AS category_amount
+                                 (
+                                 SELECT      -- categories with their amounts
+                                    c1.category_id
+                                    ,SUM(p1.amount)          AS category_amount
                                  FROM
-                                    category c1
-                                    INNER JOIN film_category fc1 ON c1.category_id = fc1.category_id
-                                    INNER JOIN inventory i1 ON fc1.film_id = i1.inventory_id
-                                    INNER JOIN rental r1 ON i1.inventory_id = r1.inventory_id
-                                    INNER JOIN payment p1 ON p1.rental_id = r1.rental_id
+                                    category                 AS c1
+                                    INNER JOIN film_category AS fc1
+                                        ON c1.category_id = fc1.category_id
+                                    INNER JOIN inventory     AS i1
+                                        ON fc1.film_id = i1.inventory_id
+                                    INNER JOIN rental        AS r1
+                                        ON i1.inventory_id = r1.inventory_id
+                                    INNER JOIN payment       AS p1
+                                        ON p1.rental_id = r1.rental_id
                                  GROUP BY
-                                    c1.category_id) cam ON cam.category_id = c2.category_id)
+                                    c1.category_id
+                                 )                           AS cam
+                                     ON cam.category_id = c2.category_id
+                         )
 ;
 
 
@@ -91,8 +110,9 @@ HAVING SUM(p3.amount) = (SELECT
 */
 SELECT
     f.title
-FROM film f
-    LEFT JOIN inventory i ON f.film_id = i.film_id
+FROM film               AS f
+    LEFT JOIN inventory AS i
+        ON f.film_id = i.film_id
 WHERE i.inventory_id IS NULL
 ;
 
@@ -104,11 +124,15 @@ WHERE i.inventory_id IS NULL
 SELECT
 	CONCAT(a.last_name, ', ', a.first_name) AS actors
 FROM
-	actor a
-	INNER JOIN film_actor fa ON a.actor_id = fa.actor_id
-    INNER JOIN film_category fc ON fc.film_id = fa.film_id
-    INNER JOIN category c ON c.category_id = fc.category_id
-    INNER JOIN film f ON f.film_id = fa.film_id
+	actor                                   AS a
+	INNER JOIN film_actor                   AS fa
+	    ON a.actor_id = fa.actor_id
+    INNER JOIN film_category                AS fc
+        ON fc.film_id = fa.film_id
+    INNER JOIN category                     AS c
+        ON c.category_id = fc.category_id
+    INNER JOIN film                         AS f
+        ON f.film_id = fa.film_id
 WHERE
     c.name = 'Children'
 GROUP BY
