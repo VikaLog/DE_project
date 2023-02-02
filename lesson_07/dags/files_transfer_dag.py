8d47cc894b4166a0617828d256c495b0d1a51f23
MSI <MSI@MacBook-Pro-7.local>
2023-02-02 21:09:49 +0200

    file_transfer_dag script addad

---
+++ lesson_07/dags/files_transfer_dag.py
@@ -0,0 +1,46 @@
+from airflow import DAG
+
+from datetime import datetime
+
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+
+
+DEFAULT_ARGS = {
+    'depends_on_past': False,
+    'email': ['admin@example.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 2,
+    'retry_delay': 30,
+}
+
+
+dag = DAG(
+    dag_id="files_transfer",
+    start_date=datetime(2022, 8, 1),
+    end_date=datetime(2022, 8, 2),
+    schedule_interval="0 0 * * *",
+    catchup=True,
+    default_args=DEFAULT_ARGS,
+    max_active_runs=1,
+)
+
+
+copy_file = LocalFilesystemToGCSOperator(
+    task_id='copy_file',
+    dag=dag,
+    src="/Users/MSI/PycharmProjects/DE_project/lesson_10/sales/2022-08-02/*.csv",
+    dst="scr1/sales/v1/{{ macros.ds_format(ds,'%Y-%m-%d' ,'%Y/%m/%d') }}/",
+    bucket='de07_bucket_02_01_2023',
+)
+
+
+task1 = EmptyOperator(
+    task_id='task1',
+    dag=dag,
+)
+
+
+task1 >> copy_file
+
